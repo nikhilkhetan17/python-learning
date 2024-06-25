724a78b614bee74d352a8d3586ccac9918f1c41b
nikhilkhetan22 <nikhilkhetan22@gmail.com>
2024-06-25 11:49:26 +0530

    if-else

--- 07_user_input.py
+++ 07_user_input.py
@@ -3,4 +3,12 @@ name = input("What's your name?: ")
 
 age= int(input("Your age: "))
 
-print(f'{name, age}')
+print(f'Wecome {name} your age is {age}')
+
+print('Program to print sum of two numbers')
+
+num1 = int(input('Enter first number: ')) # type casting to int
+num2 = int(input('Enter second number: '))
+
+result = num1 + num2
+print(f'Sum is {result}')
---
+++ 08_ifElse.py
@@ -0,0 +1,44 @@
+if False:
+    print('It is true')
+else:
+    print('It is False')
+    num1 = 55; num2 = 60
+    print(num1 + num2)
+
+
+age = 19
+
+if age >= 18:
+    print('You are major')
+else:
+    print('You are minor')
+
+# nums = int(input('Enter a number: '))
+
+# if nums % 2 == 0:
+#     print('Number is even')
+# else:
+#     print('Number is odd')
+    
+
+users = ['rahul', 'nikhil', 'anurag']
+
+if 'rahul' in users:
+    print('user exist')
+else:
+    print('User does not exist')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
