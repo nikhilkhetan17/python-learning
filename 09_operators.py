acc81b1e0c44d5257e0c21e879f8d2ec9b20ade6
nikhilkhetan22 <nikhilkhetan22@gmail.com>
2024-06-25 12:15:19 +0530

    operatorsAndNestedif-else

---
+++ 09_operators.py
@@ -0,0 +1,38 @@
+# ==
+# >=
+# >=
+# !=
+# >
+# <
+
+#elif --> else-if
+
+marks = 65
+
+if marks > 70:
+     print('Grade A')
+elif marks > 60:
+     print('Grade B')
+else:
+     print('Grade C')
+
+
+
+# Nested if-else
+
+age1 = 22
+
+if age1 > 18:
+    print('You are major')
+    if age1 >21:
+        print('Your can drink')
+    else:
+        print('You cannot drink')
+else:
+    print('you are minor')
+
+
+
+
+
+
