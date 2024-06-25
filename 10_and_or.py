2842ae6b77a411faa3f94403a9f1dfcb577547e2
nikhilkhetan22 <nikhilkhetan22@gmail.com>
2024-06-25 13:16:37 +0530

    for loop

---
+++ 10_and_or.py
@@ -0,0 +1,20 @@
+age = 17
+
+voterId = True
+
+if age >=18 and voterId:
+    print('You can vote')
+else:
+    print('You cannot vote')
+
+
+
+marks = 45; passMark = False
+
+if marks >= 50 or passMark:
+    print('You are Pass')
+else:
+    print('You are Failed')
+
+
+print(not True) # Logical not
---
+++ 11_for_loop.py
@@ -0,0 +1,31 @@
+# for i in 10, 20, 30, 40, 50:
+#     print(i)
+
+
+userName = ['nikhil', 'rahul', 'jeevesh']
+
+for ele in userName:
+    print(ele)
+
+# for x in "banana":
+#   print(x)
+
+
+dictionary = {
+    'FullName': 'Nikhil Khetan',
+     'Age': 25,
+     'City': 'raipur'
+}
+
+for key, val in dictionary.items():
+    print(f'key: {key} and value: {val}')
+
+
+for val in dictionary.values(): # same for keys -> dictionary.keys()
+    print(f'Value is: {val}')
+
+# print numbers from 1 to 20:
+    
+# for i in range(1 , 21):
+#     print(i)
+        
